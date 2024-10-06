c1b1056c51ceb89b49d669380243da46f5707ff7
Jaivir Baweja <154536624+jv199768@users.noreply.github.com>
2024-10-06 09:01:16 -0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,8 +1,30 @@
+import environ
+import os
+
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+# reading .env file
+environ.Env.read_env()
+
+...
+
 ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
 			...
 		'app',
 ]
+DATABASES = {
+    # read os.environ['DATABASE_URL'] and raises
+    # ImproperlyConfigured exception if not found
+    'default': env.db(),
+
+}
+
+
 
 
